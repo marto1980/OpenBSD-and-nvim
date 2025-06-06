260b934e298b56b86fafb298cf5a5916e444e159
Martin Aleksandrov Ivanov <marto@marto.OpenBSD>
2025-06-06 09:57:46 +0300

    Initially Configure OpenBSD and nvim

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      { "<leader>v", group = "packages", icon = { icon = "" } },
+      { "<leader>N", group = "neogit", icon = { icon = "" } },
+      { "<leader>R", group = "neorg", icon = { icon = "󰠮" } },
+    },
+  },
+}
